74397bf21e1461ae340de3ea54d772006eb9887a
Ashim Dutta <ashimdutta800@gmail.com>
2025-11-19 23:44:35 +0530

    2nd class

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/registerUser.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('auth')  //auth is the route prefix for all routes in this controller
+export class AuthController {
+
+    constructor(private readonly authService: AuthService) {}
+    @Post('register')           // routes will be /auth/register
+    register(@Body() registerUserDto:RegisterDto) {
+        const result = this.authService.registerUser(registerUserDto);
+        return result
+    }
+
+}
