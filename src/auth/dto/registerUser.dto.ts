ead1c895e778c6212cfd4cb21c6d042a86cc0a3d
Ashim Dutta <ashimdutta800@gmail.com>
2025-11-23 12:56:46 +0530

    validation

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString} from 'class-validator'
+
+
 export class RegisterDto{
+
+    @IsNotEmpty()
+    @IsString()
     fname: string;
+
+    @IsString()
     lname: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
+    @IsString()
     password: string
 }
+
+
+//This Dto belongs to user it might be under the user folder 
